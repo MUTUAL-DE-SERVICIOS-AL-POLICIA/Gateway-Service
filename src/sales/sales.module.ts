c5809d536673cfc0a2d74df2ad2074a01b807d19
ferbautistamollo <ferbautistamollo@gmail.com>
2026-09-16 10:45:07 -0400

    ajuste de modulo de ventas

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ReportsModule } from 'src/reports/reports.module';
 import { SalesController } from './sales.controller';
 
 @Module({
-  imports: [ReportsModule],
   controllers: [SalesController],
-  providers: [],
 })
 export class SalesModule {}
